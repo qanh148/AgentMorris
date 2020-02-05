985e50d9651c9416b8e42b2df272d2b2bc381b14
Abby <AbbyNode@users.noreply.github.com>
2020-02-05 18:53:05 -0500

    Upper case file names

---
+++ Scripts/objects/FlippableSprite.js
@@ -0,0 +1,11 @@
+"use strict";
+var objects;
+(function (objects) {
+    var FlippableSprite = /** @class */ (function () {
+        function FlippableSprite() {
+        }
+        return FlippableSprite;
+    }());
+    objects.FlippableSprite = FlippableSprite;
+})(objects || (objects = {}));
+//# sourceMappingURL=FlippableSprite.js.map
---
+++ Scripts/objects/FlippableSprite.ts
@@ -0,0 +1,4 @@
+module objects {
+    export class FlippableSprite {
+    }
+}
